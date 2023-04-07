3716b5060a7de25ccb3ff1b53d8627f376dfadc9
Danel <danieldob200@gmail.com>
2023-04-07 12:58:23 -0500

    fixed services link

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -25,7 +25,7 @@ export default class Home extends Component {
           <h1>At R. Cheban Towing</h1>
           <p>We understand that having a breakdown is stressful enough. That’s why we make the process easy, affordable, and reliable.<br /> We are family owned and operated, therefore your vehicle is more than just a breakdown for us. We treat and care for our customer’s as if their vehicles were our own.</p>          
           <p>Since 2016, we learned that the best business is repeat business. We strive to make an impact in our community and<br /> continue to build long-lasting relationships with our customers.</p>
-          <Link to="/contact" className={`btn ${classes.link}`}>See Our Services</Link>        
+          <Link to="/services" className={`btn ${classes.link}`}>See Our Services</Link>        
         </div>
         <div className={classes.details}>
           <img src={image2} />
